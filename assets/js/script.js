7c02109fca19847e6191cf87b5fe9759a9ccdd4b
Sidiq Hersi <hersisidiq@gmail.com>
2023-11-20 19:26:29 +0000

    adding more lines of code script which shows the forecast of the day also in the css file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,73 @@
-// this is an event listener for when the search button is pressed
-document.addEventListener("DOMContentLoaded", function () {
+$(document).ready(function () {
   // Event listener for the form submission
-  document
-    .getElementById("search-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
+  $("#search-form").submit(function (event) {
+    event.preventDefault();
 
-      // Get the city name from the input field
-      var cityName = document
-        .getElementById("search-input")
-        .ariaValueMax.trim();
+    // Get the city name from the input field
+    var cityName = $("#search-input").val().trim();
 
-      // clear the input field
-      document.getElementById("search-input").value = "";
+    // Call your weather API function with the cityName parameter
+    getWeatherData(cityName);
 
-      //Update the search history
-      updateSearchHistory(cityName);
+    // Clear the input field
+    $("#search-input").val("");
+
+    // Update the search history
+    updateSearchHistory(cityName);
+  });
+
+  // Function to fetch the weather data from the API
+  function getWeatherData(cityName) {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+    $.ajax({
+      url: apiUrl,
+      method: "GET",
+      dataType: "json",
+      success: function (data) {
+        updateTodaySection(data);
+        console.log(getWeatherData);
+      },
     });
-});
+  }
 
-function getWeatherData(cityName){
-  
-}
+  function updateTodaySection(weatherData) {
+    // Update the 'today' section with the relevant weather information
+    var todaySection = $("#today");
+    // Get the current date using Day.js
+    var currentDate = dayjs().format("DD/MM/YYYY");
+    // Construct the icon URL based on the weather condition code
+    var weatherIconUrl = getWeatherIconUrl(weatherData.weather[0].icon);
+     // Convert wind speed from m/s to kph
+     var windSpeedKph = (weatherData.wind.speed * 3.6).toFixed(2);
+
+    todaySection.html(`
+          <h2>${weatherData.name} (${currentDate}) <img src="${weatherIconUrl}" alt="Weather Icon" class="weather-icon" /> </h2>
+          <p>Temperature: ${weatherData.main.temp} °C</p>
+          <p>Weather: ${weatherData.weather[0].description}</p>
+          <p>Wind: ${windSpeedKph} KPH</p>
+          <p>Humidity: ${weatherData.main.humidity}%</p>
+        `);
+  }
+  // Function to get the URL of the weather icon based on weather condition
+  function getWeatherIconUrl(iconCode) {
+    // Adjust the URL format based on OpenWeatherMap's icon URLs
+    return `https://openweathermap.org/img/w/${iconCode}.png`;
+  }
+
+  // Function to update the search history
+  function updateSearchHistory(cityName) {
+    // Create a list item element with the city name
+    var listItem = $("<a>")
+      .addClass("list-group-item list-group-item-action")
+      .text(cityName);
+
+    // Add a click event listener to the list item for re-searching
+    listItem.click(function () {
+      getWeatherData(cityName);
+    });
+
+    // Add the list item to the 'history' list
+    $("#history").prepend(listItem);
+  }
+});
